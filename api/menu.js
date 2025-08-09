b7b89fc77ae17979bafc9025b6f8995d4aa7707c
hithamkasim1122 <hithamkasim@gmail.com>
2025-08-09 06:40:02 +0300

    Update menu.js

--- api/menu.js
+++ api/menu.js
@@ -1,13 +1,16 @@
+import fetch from 'node-fetch';
+
 export default async function handler(req, res) {
   try {
     const response = await fetch('https://pt.hqasite.com/abu_haitham/wep/api/menu.php');
     if (!response.ok) {
+      console.error('Response status:', response.status);
       throw new Error(`خطأ في استرجاع البيانات: ${response.status}`);
     }
     const data = await response.json();
     res.status(200).json(data);
   } catch (error) {
-    console.error('حدث خطأ:', error.message);
+    console.error('حدث خطأ في API /api/menu:', error);
     res.status(500).json({ message: 'حدث خطأ عند تحميل البيانات.' });
   }
 }
